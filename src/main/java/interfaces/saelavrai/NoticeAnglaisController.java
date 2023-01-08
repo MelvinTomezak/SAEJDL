a2cec8022f23153d85abbdbfa8f33f81de9a2c6a
MelvinTomezak <melvin.tomezak@etu.univ-amu.fr>
2023-01-08 19:08:13 +0100

    Création de la notice en anglais et la traduction en français

--- src/main/java/interfaces/saelavrai/NoticeAnglaisController.java
+++ src/main/java/interfaces/saelavrai/NoticeAnglaisController.java
@@ -1,25 +1,20 @@
 package interfaces.saelavrai;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
-public class NoticeController {
+public class NoticeAnglaisController {
     @FXML
-    /**
-     * La fonction Home est reliée au button Home de la page Notice et elle va permettre de revenir à la page d'Accueil.
-     */
-    public void Home (){
+    public void Home(){
         AccueilMain.stackPane.getChildren().get(0).setVisible(false);
         AccueilMain.stackPane.getChildren().get(1).setVisible(true);
         AccueilMain.stackPane.getChildren().get(2).setVisible(false);
         AccueilMain.stackPane.getChildren().get(3).setVisible(false);
     }
     @FXML
-    public void Translate() {
-        AccueilMain.stackPane.getChildren().get(0).setVisible(false);
+    public void Translate(){
+        AccueilMain.stackPane.getChildren().get(0).setVisible(true);
         AccueilMain.stackPane.getChildren().get(1).setVisible(false);
         AccueilMain.stackPane.getChildren().get(2).setVisible(false);
-        AccueilMain.stackPane.getChildren().get(3).setVisible(true);
+        AccueilMain.stackPane.getChildren().get(3).setVisible(false);
     }
 }
--- src/main/java/interfaces/saelavrai/ScoreController.java
+++ src/main/java/interfaces/saelavrai/ScoreController.java
@@ -1,21 +1,14 @@
 package interfaces.saelavrai;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-import java.awt.*;
-
 public class ScoreController {
     @FXML
-    private Button button;
-
     /**
      * La fonction Home est reliée au button Home de la page score et elle permet de revenir à la page d'Accueil.
-     * @param actionEvent
      */
-    public void Home (ActionEvent actionEvent) {
+    public void Home () {
         AccueilMain.stackPane.getChildren().get(0).setVisible(false);
         AccueilMain.stackPane.getChildren().get(1).setVisible(true);
         AccueilMain.stackPane.getChildren().get(2).setVisible(false);
+        AccueilMain.stackPane.getChildren().get(3).setVisible(false);
     }
 }

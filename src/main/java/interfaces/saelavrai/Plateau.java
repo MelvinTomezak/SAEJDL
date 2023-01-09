a057beb1d5434e7ab4403b58d7e3e54c10155c6d
MelvinTomezak <melvin.tomezak@etu.univ-amu.fr>
2023-01-09 23:04:35 +0100

    Creation des pions + debut de mouvement de pion en fonction du dé

--- src/main/java/interfaces/saelavrai/Plateau.java
+++ src/main/java/interfaces/saelavrai/Plateau.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -17,20 +18,31 @@ import java.util.Random;
 public class Plateau extends Application {
     private int de;
     private Random random = new Random();
+    private boolean FinTourJoueur1;
+    private boolean FinTourJoueur2;
+    private boolean FinTourJoueur3;
+    private boolean FinTourJoueur4;
+    private int ligne = 9;
+    private int colonne = 0;
+    Circle pion1 = new Circle(10, Color.PURPLE);
+    Circle pion2 = new Circle(10, Color.BLACK);
+    Circle pion3 = new Circle(10, Color.CYAN);
+    Circle pion4 = new Circle(10, Color.BLUE);
+    GridPane plateau = new GridPane();
+    Rectangle rect;
     @Override
     public void start(Stage stage) {
-        GridPane plateau = new GridPane();
         Button De = new Button("Lancer le dé");
         De.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 LancerDe();
-                System.out.println("Vous avez fait " + LancerDe());
+                mouvementPion();
             }
         });
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                Rectangle rect = new Rectangle(50, 50);
+                rect = new Rectangle(50,50);
                 if (i % 2 == 0) {
                     if (j % 2 == 0) {
                         rect.setFill(Color.GREEN);
@@ -56,6 +68,18 @@ public class Plateau extends Application {
                 plateau.setAlignment(Pos.CENTER);
             }
         }
+        plateau.add(pion1,colonne,ligne);
+        plateau.add(pion2,colonne,ligne);
+        plateau.add(pion3,colonne,ligne);
+        plateau.add(pion4,colonne,ligne);
+        pion1.setTranslateX(15);
+        pion1.setTranslateY(15);
+        pion2.setTranslateX(15);
+        pion2.setTranslateY(-15);
+        pion3.setTranslateX(0);
+        pion3.setTranslateY(0);
+        pion4.setTranslateX(30);
+        pion4.setTranslateY(0);
         VBox vBox = new VBox();
         vBox.setAlignment(Pos.CENTER_LEFT);
         vBox.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
@@ -68,9 +92,24 @@ public class Plateau extends Application {
     }
 
     public int LancerDe (){
-        this.de = 1+this.random.nextInt(6);
-
-        return getDe();
+        this.de = this.random.nextInt(6) + 1;
+        return this.de;
+    }
+    public void mouvementPion(){
+        while(pion1.getTranslateX() < 515){
+            System.out.println("Vous avez fait " + getDe());
+            pion1.setTranslateX(pion1.getTranslateX() + (50*getDe()));
+            FinTourJoueur1 = true;
+            if (FinTourJoueur1 == true){
+                System.out.println("Fin tour joueur 1");
+                break;
+            }
+            if (pion1.getTranslateX() >= 515){
+                pion1.setTranslateY(pion1.getTranslateY() - 50);
+                pion1.setTranslateX(pion1.getTranslateX() - (50*getDe()));
+                System.out.println("Ligne 1 fini");
+            }
+        }
     }
 
     public int getDe() {
